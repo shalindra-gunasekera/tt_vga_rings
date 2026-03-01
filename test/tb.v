1274c573d44e383f2c321a867f3b9ab93ee9b911
Shalindra Gunasekera <shali.gunasekera@gmail.com>
2026-03-01 14:15:32 +1100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_shalindra_vga_rings (
+  tt_um_shalindra_vga_rings user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
